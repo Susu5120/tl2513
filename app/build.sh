6bfedd32377b91a6a201181a58ac45ae3a58a9b1
Suyunmeng <hua-honkai@qq.com>
2024-10-06 18:38:02 +0800

    update

--- app/build.sh
+++ app/build.sh
@@ -1,15 +1,3 @@
 npm install -g pm2
 cd /app
 npm install -r package.json
-cd /app/blumtod
-python3 -m venv venv
-/app/blumtod/venv/bin/pip3 install --upgrade pip setuptools wheel
-/app/blumtod/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/blumtod/requirements.txt
-cd /app/MemeFiBot
-python3 -m venv venv
-/app/MemeFiBot/venv/bin/pip3 install --upgrade pip setuptools wheel
-/app/MemeFiBot/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/MemeFiBot/requirements.txt
-cd /app/Tomarket
-python3 -m venv venv
-/app/Tomarket/venv/bin/pip3 install --upgrade pip setuptools wheel
-/app/Tomarket/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/Tomarket/requirements.txt
--- app/start.sh
+++ app/start.sh
@@ -1,3 +1,16 @@
+cd /app/blumtod
+python3 -m venv venv
+/app/blumtod/venv/bin/pip3 install --upgrade pip setuptools wheel
+/app/blumtod/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/blumtod/requirements.txt
+cd /app/MemeFiBot
+python3 -m venv venv
+/app/MemeFiBot/venv/bin/pip3 install --upgrade pip setuptools wheel
+/app/MemeFiBot/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/MemeFiBot/requirements.txt
+cd /app/Tomarket
+python3 -m venv venv
+/app/Tomarket/venv/bin/pip3 install --upgrade pip setuptools wheel
+/app/Tomarket/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/Tomarket/requirements.txt
+cd /app
 pm2 start /app/index.js --log /dev/null
 pm2 start /app/blumtod/start.sh --name Blum --log /app/blum.log
 pm2 start /app/MemeFiBot/start.sh --name MemeFi --log /app/MemeFi.log
