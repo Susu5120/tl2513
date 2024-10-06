47edac56f22aed9aa8d93e892279703059ef26d5
Suyunmeng <hua-honkai@qq.com>
2024-10-06 18:47:35 +0800

    update

--- app/build.sh
+++ app/build.sh
@@ -1,3 +1,4 @@
 npm install -g pm2
 cd /app
 npm install -r package.json
+chmod +x /app/blumtod/start.sh /app/MemeFiBot/start.sh /app/Tomarket/start.sh
--- app/start.sh
+++ app/start.sh
@@ -12,6 +12,7 @@ python3 -m venv venv
 /app/Tomarket/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/Tomarket/requirements.txt
 cd /app
 pm2 start /app/index.js --log /dev/null
+sleep 30s
 pm2 start /app/blumtod/start.sh --name Blum --log /app/blum.log
 pm2 start /app/MemeFiBot/start.sh --name MemeFi --log /app/MemeFi.log
 pm2 start /app/Tomarket/start.sh --name Tomarket --log /app/Tomarket.log
