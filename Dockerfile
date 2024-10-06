8d2d1589a941170e5cd14cfacc5d71863d3d8eef
Suyunmeng <hua-honkai@qq.com>
2024-10-06 20:34:48 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -17,6 +17,18 @@ COPY app/ /app/
 RUN cd /app &&\
      apt-get update && apt-get install -y supervisor &&\
      npm install -r package.json &&\
+     cd /app/blumtod &&\
+     python3 -m venv venv &&\
+     /app/blumtod/venv/bin/pip3 install --upgrade pip setuptools wheel &&\
+     /app/blumtod/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/blumtod/requirements.txt &&\
+     cd /app/MemeFiBot &&\
+     python3 -m venv venv &&\
+     /app/MemeFiBot/venv/bin/pip3 install --upgrade pip setuptools wheel &&\
+     /app/MemeFiBot/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/MemeFiBot/requirements.txt &&\
+     cd /app/Tomarket &&\
+     python3 -m venv venv &&\
+     /app/Tomarket/venv/bin/pip3 install --upgrade pip setuptools wheel &&\
+     /app/Tomarket/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/Tomarket/requirements.txt &&\
      chmod +x /app/supervisord.conf
 
 COPY /app/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
--- app/start.sh
+++ app/start.sh
@@ -1,13 +1,13 @@
-cd /app/blumtod
-python3 -m venv venv
-/app/blumtod/venv/bin/pip3 install --upgrade pip setuptools wheel
-/app/blumtod/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/blumtod/requirements.txt
-cd /app/MemeFiBot
-python3 -m venv venv
-/app/MemeFiBot/venv/bin/pip3 install --upgrade pip setuptools wheel
-/app/MemeFiBot/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/MemeFiBot/requirements.txt
-cd /app/Tomarket
-python3 -m venv venv
-/app/Tomarket/venv/bin/pip3 install --upgrade pip setuptools wheel
-/app/Tomarket/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/Tomarket/requirements.txt
+#cd /app/blumtod
+#python3 -m venv venv
+#/app/blumtod/venv/bin/pip3 install --upgrade pip setuptools wheel
+#/app/blumtod/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/blumtod/requirements.txt
+#cd /app/MemeFiBot
+#python3 -m venv venv
+#/app/MemeFiBot/venv/bin/pip3 install --upgrade pip setuptools wheel
+#/app/MemeFiBot/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/MemeFiBot/requirements.txt
+#cd /app/Tomarket
+#python3 -m venv venv
+#/app/Tomarket/venv/bin/pip3 install --upgrade pip setuptools wheel
+#/app/Tomarket/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/Tomarket/requirements.txt
 /usr/bin/supervisord
