7a9a5a43f10cda73b75701db81c0bf7ca76a0159
Suyunmeng <hua-honkai@qq.com>
2024-10-07 21:25:00 +0800

    update

--- app/start.sh
+++ app/start.sh
@@ -1,19 +1,19 @@
 cd /app
 pm2 start /app/warp/start.sh --name Wireproxy --log /dev/null
 pm2 start /app/index.js --log /dev/null
-#cd /app/blumtod
+cd /app/blumtod
 #python3 -m venv venv
-#/app/blumtod/venv/bin/pip3 install --upgrade pip setuptools wheel
-#/app/blumtod/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/blumtod/requirements.txt
+/app/blumtod/venv/bin/pip3 install --upgrade pip setuptools wheel
+/app/blumtod/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/blumtod/requirements.txt
 pm2 start /app/blumtod/start.sh --name Blum --log /app/blum.log
 #cd /app/MemeFiBot
 #python3 -m venv venv
-#/app/MemeFiBot/venv/bin/pip3 install --upgrade pip setuptools wheel
-#/app/MemeFiBot/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/MemeFiBot/requirements.txt
+/app/MemeFiBot/venv/bin/pip3 install --upgrade pip setuptools wheel
+/app/MemeFiBot/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/MemeFiBot/requirements.txt
 pm2 start /app/MemeFiBot/start.sh --name MemeFi --log /app/MemeFi.log
-#cd /app/Tomarket
+cd /app/Tomarket
 #python3 -m venv venv
-#/app/Tomarket/venv/bin/pip3 install --upgrade pip setuptools wheel
-#/app/Tomarket/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/Tomarket/requirements.txt
+/app/Tomarket/venv/bin/pip3 install --upgrade pip setuptools wheel
+/app/Tomarket/venv/bin/pip3 install --no-warn-script-location --no-cache-dir -r /app/Tomarket/requirements.txt
 pm2 start /app/Tomarket/start.sh --name Tomarket --log /app/Tomarket.log
 sleep 20d
